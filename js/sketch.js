f35c720ee9d8fc5702bd4e1ad9c3d7e27cdbfcf7
Oscar <etherealflux94@gmail.com>
2016-10-04 15:48:05 -0500

    El p5.js

---
+++ js/sketch.js
@@ -0,0 +1,29 @@
+var ancho = window.innerWidth - 17;
+var altura = 150;
+
+var fuente;
+
+function preload() {
+    fuente = loadFont("js/OpenSans-Regular.ttf");
+}
+
+function setup() {
+    var cnv = createCanvas(ancho, altura); //El -17 es para que se acomode bien al ancho de la pantalla
+    cnv.parent("p5");
+
+    frameRate(24);
+
+    smooth();
+    textFont(fuente);
+    textSize(200);
+    textAlign(CENTER);
+
+}
+
+function draw() {
+    background(255);
+    var mausX = map(mouseX, 0, 100, 0, 100);
+    textSize(mausX);
+    fill(0);
+    text("RESONAR", ancho / 2, (altura / 2) + 40);
+}
