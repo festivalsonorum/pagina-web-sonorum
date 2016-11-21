160c96da8a5ce0d1b5adf7e675f3be680bbd0a45
Oscar <etherealflux94@gmail.com>
2016-11-21 12:38:55 -0600

    Un chingo de imágenes

---
+++ js/jq.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.parent-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image'
+        // other options
+    });
+});
